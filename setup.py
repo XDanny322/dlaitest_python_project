1f20ebc7617d7d62de422d69e3bb12e4f303830c
Danny Lai <hlai@factset.com>
2020-08-03 23:02:34 -0400

    First push

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="dlaitest_python_project",  # How you named your package folder (MyLib)
     packages=["dlaitest_python_project"],  # Chose the same as "name"
-    version="0.0.2",  # Start with a small number and increase it with every change you make
+    version="0.0.3",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="TYPE YOUR DESCRIPTION HERE",  # Give a short description about your library
     author="Danny Lai",  # Type in your name
